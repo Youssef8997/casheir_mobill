9fa6a0015b40bff6715e6a33d1dc2d8644b8a1ea
Youssef8997 <67810732+Youssef8997@users.noreply.github.com>
2022-04-28 06:28:13 +0200

    first step in empo screen

--- lib/Models/Empo.dart
+++ lib/Models/Empo.dart
@@ -2,8 +2,7 @@ import 'package:casheir_mobill/Cuibt/State.dart';
 import 'package:casheir_mobill/Cuibt/cuibt.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_simple_rating_bar/flutter_simple_rating_bar.dart';
-
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 class Employees extends StatelessWidget {
 
   @override
@@ -83,7 +82,47 @@ children: [
       padding: const EdgeInsets.all(15),
       child: Column(
         children: [
-        ],
+
+        RatingBar.builder(
+        initialRating: 4,
+        itemCount: 5,
+        allowHalfRating: true,
+        itemBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return Icon(
+                Icons.sentiment_very_dissatisfied,
+                color: Colors.red,
+              );
+            case 1:
+              return Icon(
+                Icons.sentiment_dissatisfied,
+                color: Colors.redAccent,
+              );
+            case 2:
+              return Icon(
+                Icons.sentiment_neutral,
+                color: Colors.amber,
+              );
+            case 3:
+              return Icon(
+                Icons.sentiment_satisfied,
+                color: Colors.lightGreen,
+              );
+            case 4:
+              return Icon(
+                Icons.sentiment_very_satisfied,
+                color: Colors.green,
+              );
+            default:
+              return SizedBox.shrink();
+          }
+        },
+        onRatingUpdate: (rating) {
+
+        },
+itemPadding: const EdgeInsets.symmetric(horizontal: 5.0),
+        )],
       ),
     );
   }
